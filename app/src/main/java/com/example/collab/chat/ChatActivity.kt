c71e18e981ac3f519f4f81b2c21cb5c0b3ff1e1b
Volha Zinkevich <o.zinkevich@icloud.com>
2022-12-21 23:20:46 +0100

    ChatActivity

---
+++ app/src/main/java/com/example/collab/chat/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.example.collab.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.collab.R
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
